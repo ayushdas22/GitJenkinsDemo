8d97732cca723136b482a15347f05b331f3f66c9
Ayush Das <55491446+ayushdas22@users.noreply.github.com>
2025-07-03 17:48:36 +0530

    values updated 2

--- Calculator.java
+++ Calculator.java
@@ -2,7 +2,7 @@ public class Calculator {
 
 	public static void main(String[] args) {
 
-		int a = 100, b = 200;
+		int a = 100, b = 500;
 		System.out.println("Sum of " + a + " and " + b + " is : " + (a + b));
 	}
 
